23d8f84f113ce9e87e5b5ed521a3d9e24f548067
Dmitry Kolesnikovich <dmitrykolesnikovich@users.noreply.github.com>
2024-04-19 17:47:20 +0300

    art

---
+++ res/index.ts
@@ -0,0 +1,241 @@
+const backgroundMp3 = "./audio/background.mp3"
+const clickEnabledMp3 = "./audio/clickEnabled.mp3"
+const clickDisabledMp3 = "./audio/clickDisabled.mp3"
+const coinsMp3 = "./audio/coins.mp3"
+const dialogHideMp3 = "./audio/dialogHide.mp3"
+const dialogShowMp3 = "./audio/dialogShow.mp3"
+const downgradeMp3 = "./audio/downgrade.mp3"
+const loseMp3 = "./audio/lose.mp3"
+const purchaseMp3 = "./audio/purchase.mp3"
+const rainMp3 = "./audio/rain.mp3"
+const streakDynamiteMp3 = "./audio/streakDynamite.mp3"
+const streakGroupMp3 = "./audio/streakGroup.mp3"
+const streakMixMp3 = "./audio/streakMix.mp3"
+const streakResetMp3 = "./audio/streakReset.mp3"
+const streakUndefinedMp3 = "./audio/streakUndefined.mp3"
+const streakVerticalOrHorizontalMp3 = "./audio/streakVerticalOrHorizontal.mp3"
+const winMp3 = "./audio/win.mp3"
+const fredokaOneTtf = "./fonts/fredokaOne.ttf"
+const insanibuTtf = "./fonts/insanibu.ttf"
+import avatarBorderPng from "./images/avatar/avatarBorder.png"
+import avatarBorderShadowPng from "./images/avatar/avatarBorderShadow.png"
+import avatarBoyPng from "./images/avatar/avatarBoy.png"
+import avatarGirlPng from "./images/avatar/avatarGirl.png"
+import avatarIndicatorPng from "./images/avatar/avatarIndicator.png"
+import avatarIndicatorShinePng from "./images/avatar/avatarIndicatorShine.png"
+import background1Jpg from "./images/background/background1.jpg"
+import background2Jpg from "./images/background/background2.jpg"
+import background3Jpg from "./images/background/background3.jpg"
+import background4Jpg from "./images/background/background4.jpg"
+import background5Jpg from "./images/background/background5.jpg"
+import backgroundGameJpg from "./images/background/backgroundGame.jpg"
+import boosterColorBombPng from "./images/booster/boosterColorBomb.png"
+import boosterDynamitePng from "./images/booster/boosterDynamite.png"
+import boosterHorizontalBombPng from "./images/booster/boosterHorizontalBomb.png"
+import boosterVerticalBombPng from "./images/booster/boosterVerticalBomb.png"
+import buttonBoosterPng from "./images/button/buttonBooster.png"
+import buttonCheckboxPng from "./images/button/buttonCheckbox.png"
+import buttonCheckboxDisabled from "./images/button/buttonCheckboxDisabled.png"
+import buttonCircleBluePng from "./images/button/buttonCircleBlue.png"
+import buttonCircleGreenPng from "./images/button/buttonCircleGreen.png"
+import buttonCircleGreyPng from "./images/button/buttonCircleGrey.png"
+import buttonCircleRedPng from "./images/button/buttonCircleRed.png"
+import buttonClosePng from "./images/button/buttonClose.png"
+import buttonIndicatorPng from "./images/button/buttonIndicator.png"
+import buttonLevelPng from "./images/button/buttonLevel.png"
+import buttonLevelDisabledPng from "./images/button/buttonLevelDisabled.png"
+import buttonLevelShadowPng from "./images/button/buttonLevelShadow.png"
+import buttonLevelShinePng from "./images/button/buttonLevelShine.png"
+import buttonRadioPng from "./images/button/buttonRadio.png"
+import buttonRectangleBluePng from "./images/button/buttonRectangleBlue.png"
+import buttonRectangleGreenPng from "./images/button/buttonRectangleGreen.png"
+import buttonRectangleGreenDarkPng from "./images/button/buttonRectangleGreenDark.png"
+import buttonRectangleGreenSmallPng from "./images/button/buttonRectangleGreenSmall.png"
+import buttonRectanglePinkPng from "./images/button/buttonRectanglePink.png"
+import buttonRectangleRedPng from "./images/button/buttonRectangleRed.png"
+import buttonRectangleShadowPng from "./images/button/buttonRectangleShadow.png"
+import cloud1Png from "./images/cloud/cloud1.png"
+import cloud2Png from "./images/cloud/cloud2.png"
+import cloud3Png from "./images/cloud/cloud3.png"
+import cloudLevelPng from "./images/cloud/cloudLevel.png"
+import cloudLosePng from "./images/cloud/cloudLose.png"
+import coins1Png from "./images/coins/coins1.png"
+import coins2Png from "./images/coins/coins2.png"
+import coins3Png from "./images/coins/coins3.png"
+import coins4Png from "./images/coins/coins4.png"
+import coins5Png from "./images/coins/coins5.png"
+import coins6Png from "./images/coins/coins6.png"
+import iconArrowBackPng from "./images/icon/iconArrowBack.png"
+import iconBackwardPng from "./images/icon/iconBackward.png"
+import iconExitPng from "./images/icon/iconExit.png"
+import iconForwardPng from "./images/icon/iconForward.png"
+import iconHelpPng from "./images/icon/iconHelp.png"
+import iconInfoPng from "./images/icon/iconInfo.png"
+import iconLevelPng from "./images/icon/iconLevel.png"
+import iconMorePng from "./images/icon/iconMore.png"
+import iconMusicPng from "./images/icon/iconMusic.png"
+import iconNextPng from "./images/icon/iconNext.png"
+import iconRepeatPng from "./images/icon/iconRepeat.png"
+import iconResetPng from "./images/icon/iconReset.png"
+import iconSettingsPng from "./images/icon/iconSettings.png"
+import iconSoundPng from "./images/icon/iconSound.png"
+import life1Png from "./images/life/life1.png"
+import life1GreyPng from "./images/life/life1Grey.png"
+import life1ShadowPng from "./images/life/life1Shadow.png"
+import life3Png from "./images/life/life3.png"
+import lifeBrokenPng from "./images/life/lifeBroken.png"
+import lifeBrokenShadowPng from "./images/life/lifeBrokenShadow.png"
+import blockPng from "./images/misc/block.png"
+import dividerPng from "./images/misc/divider.png"
+import logoPng from "./images/misc/logo.png"
+import playPng from "./images/misc/play.png"
+import progressBarPng from "./images/misc/progressBar.png"
+import panelAlert1Png from "./images/panel/panelAlert1.png"
+import panelAlert2Png from "./images/panel/panelAlert2.png"
+import panelBoostersPng from "./images/panel/panelBoosters.png"
+import panelCoinsPng from "./images/panel/panelCoins.png"
+import panelFlagBluePng from "./images/panel/panelFlagBlue.png"
+import panelFlagGreyPng from "./images/panel/panelFlagGrey.png"
+import panelGoalPng from "./images/panel/panelGoal.png"
+import panelHeadlinePng from "./images/panel/panelHeadline.png"
+import panelHeadlineDarkPng from "./images/panel/panelHeadlineDark.png"
+import panelInfoPng from "./images/panel/panelInfo.png"
+import panelListPng from "./images/panel/panelList.png"
+import panelMovesPng from "./images/panel/panelMoves.png"
+import panelProgressPng from "./images/panel/panelProgress.png"
+import panelTitlePng from "./images/panel/panelTitle.png"
+import particlesBlockPng from "./images/particles/particlesBlock.png"
+import particlesColorBombPng from "./images/particles/particlesColorBomb.png"
+import particlesGlitterPng from "./images/particles/particlesGlitter.png"
+import particlesRainPng from "./images/particles/particlesRain.png"
+import particlesShinePng from "./images/particles/particlesShine.png"
+import particlesShineRadial1Png from "./images/particles/particlesShineRadial1.png"
+import particlesShineRadial2Png from "./images/particles/particlesShineRadial2.png"
+import particlesShineRadial3Png from "./images/particles/particlesShineRadial3.png"
+import particlesShineRadial4Png from "./images/particles/particlesShineRadial4.png"
+import starBigGreyPng from "./images/star/starBigGrey.png"
+import starBigYellowPng from "./images/star/starBigYellow.png"
+import starSmallGreyPng from "./images/star/starSmallGrey.png"
+import starSmallYellowPng from "./images/star/starSmallYellow.png"
+
+export {
+    backgroundMp3,
+    clickEnabledMp3,
+    clickDisabledMp3,
+    coinsMp3,
+    dialogHideMp3,
+    dialogShowMp3,
+    downgradeMp3,
+    loseMp3,
+    purchaseMp3,
+    rainMp3,
+    streakDynamiteMp3,
+    streakGroupMp3,
+    streakMixMp3,
+    streakResetMp3,
+    streakUndefinedMp3,
+    streakVerticalOrHorizontalMp3,
+    winMp3,
+    fredokaOneTtf,
+    insanibuTtf,
+    avatarBorderPng,
+    avatarBorderShadowPng,
+    avatarBoyPng,
+    avatarGirlPng,
+    avatarIndicatorPng,
+    avatarIndicatorShinePng,
+    background1Jpg,
+    background2Jpg,
+    background3Jpg,
+    background4Jpg,
+    background5Jpg,
+    backgroundGameJpg,
+    boosterColorBombPng,
+    boosterDynamitePng,
+    boosterHorizontalBombPng,
+    boosterVerticalBombPng,
+    buttonBoosterPng,
+    buttonCheckboxPng,
+    buttonCheckboxDisabled,
+    buttonCircleBluePng,
+    buttonCircleGreenPng,
+    buttonCircleGreyPng,
+    buttonCircleRedPng,
+    buttonClosePng,
+    buttonIndicatorPng,
+    buttonLevelPng,
+    buttonLevelDisabledPng,
+    buttonLevelShadowPng,
+    buttonLevelShinePng,
+    buttonRadioPng,
+    buttonRectangleBluePng,
+    buttonRectangleGreenPng,
+    buttonRectangleGreenDarkPng,
+    buttonRectangleGreenSmallPng,
+    buttonRectanglePinkPng,
+    buttonRectangleRedPng,
+    buttonRectangleShadowPng,
+    cloud1Png,
+    cloud2Png,
+    cloud3Png,
+    cloudLevelPng,
+    cloudLosePng,
+    coins1Png,
+    coins2Png,
+    coins3Png,
+    coins4Png,
+    coins5Png,
+    coins6Png,
+    iconArrowBackPng,
+    iconBackwardPng,
+    iconExitPng,
+    iconForwardPng,
+    iconHelpPng,
+    iconInfoPng,
+    iconLevelPng,
+    iconMorePng,
+    iconMusicPng,
+    iconNextPng,
+    iconRepeatPng,
+    iconResetPng,
+    iconSettingsPng,
+    iconSoundPng,
+    life1Png,
+    life1GreyPng,
+    life1ShadowPng,
+    life3Png,
+    lifeBrokenPng,
+    lifeBrokenShadowPng,
+    blockPng,
+    dividerPng,
+    logoPng,
+    playPng,
+    progressBarPng,
+    panelAlert1Png,
+    panelAlert2Png,
+    panelBoostersPng,
+    panelCoinsPng,
+    panelFlagBluePng,
+    panelFlagGreyPng,
+    panelGoalPng,
+    panelHeadlinePng,
+    panelHeadlineDarkPng,
+    panelInfoPng,
+    panelListPng,
+    panelMovesPng,
+    panelProgressPng,
+    panelTitlePng,
+    particlesBlockPng,
+    particlesColorBombPng,
+    particlesGlitterPng,
+    particlesRainPng,
+    particlesShinePng,
+    particlesShineRadial1Png,
+    particlesShineRadial2Png,
+    particlesShineRadial3Png,
+    particlesShineRadial4Png,
+    starBigGreyPng,
+    starBigYellowPng,
+    starSmallGreyPng,
+    starSmallYellowPng,
+}
